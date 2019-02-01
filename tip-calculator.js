000dc4cdf42c0ce1508f3b24331b866216c60171
Mykol2018 <43974790+Mykol2018@users.noreply.github.com>
2019-02-01 04:17:34 +0000

    Update tip-calculator.js

--- tip-calculator.js
+++ tip-calculator.js
@@ -1,36 +1,36 @@
 //Calculate Tip
 function calculateTip() {
-  var billAmt = document.getElementById("billamt").value;
-  var serviceQual = document.getElementById("serviceQual").value;
-  var numOfPeople = document.getElementById("peopleamt").value;
+  var monto_cuenta = document.getElementById("monto_cuenta").value;
+  var CualidadServicio = document.getElementById("CualidadServicio").value;
+  var NumPersonas = document.getElementById("NumPersonas").value;
 
   //validar la entrada
-  if (billAmt === "" || serviceQual == 0) {
+  if (monto_cuenta === "" || CualidadServicio == 0) {
     alert("Entrar valores");
     return;
   }
   //Vemos si el campo está vacio o inferior o igual a 1
-  if (numOfPeople === "" || numOfPeople <= 1) {
-    numOfPeople = 1;
+  if (NumPersonas === "" || NumPersonas <= 1) {
+    NumPersonas = 1;
     document.getElementById("each").style.display = "none";
   } else {
     document.getElementById("each").style.display = "block";
   }
 
   //Calcular la propina
-  var total = (billAmt * serviceQual) / numOfPeople;
+  var total = (monto_cuenta * CualidadServicio) / NumPersonas;
   //solo dos decimales
   total = Math.round(total * 100) / 100;
   //siempre guardamos 2 digitos despues del punto decimal
   total = total.toFixed(2);
   //Mostrar la propina
-  document.getElementById("totalTip").style.display = "block";
-  document.getElementById("tip").innerHTML = total;
+  document.getElementById("TotalPropina").style.display = "block";
+  document.getElementById("propina").innerHTML = total;
 
 }
 
 //Escondemos la propina al inicio
-document.getElementById("totalTip").style.display = "none";
+document.getElementById("TotalPropina").style.display = "none";
 document.getElementById("each").style.display = "none";
 
 //Llamar la función al hacer clic
